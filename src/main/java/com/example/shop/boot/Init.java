5b107b40935f8a82010b26a89c87b1d872d70565
zhazgul004 <zhazgul004@gmail.com>
2020-12-13 19:33:01 +0600

    fixed parameter orders of constructor

--- src/main/java/com/example/shop/boot/Init.java
+++ src/main/java/com/example/shop/boot/Init.java
@@ -22,6 +22,6 @@ public class Init implements CommandLineRunner {
 //            role = roleRepository.save(new RoleEntity("ROLE_SUPER_ADMIN"));
 //
 //        userService.createSuperAdmin(new
-//                UserEntity("zhazgul@gmail.com", "Zhazgul", "123456", null, role));
+//                UserEntity("zhazgul.j@gmail.com", "AAA", "123456", null, role, null));
     }
 }
